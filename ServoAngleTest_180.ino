8167848cf353765ae7a4c366f035952f420b0876
asifsaif6666 <55478736+asifsaif6666@users.noreply.github.com>
2025-05-17 18:57:34 +0530

    Update ServoAngleTest_180.ino

--- ServoAngleTest_180.ino
+++ ServoAngleTest_180.ino
@@ -15,24 +15,21 @@ void setup() {
   pwm.begin();
   pwm.setPWMFreq(50);
   delay(10);
-
+  pwm.setPWM(0, 0, angleToPulse(180));
+  delay(1000);
+ 
 }
 
 void loop() {
-  
-  for(int i=0;i<=90;i++){
+  for(int i=180;i>=90;i--){
     pwm.setPWM(0, 0, angleToPulse(i));
     delay(10);
   }
-  Serial.println("Servo 1 90 degree");
   delay(1000);
-
-  for(int i=90;i>=0;i--){
+  for(int i=90;i<=180;i++){
     pwm.setPWM(0, 0, angleToPulse(i));
     delay(10);
-  }
-  Serial.println("Servo 1 90 degree");
+  } 
   delay(1000);
-  
 
 }
