1176d807745801e4014e343764579f91428c79e0
asifsaif6666 <55478736+asifsaif6666@users.noreply.github.com>
2025-05-17 19:01:19 +0530

    Create ServoAngleTest_90.ino

---
+++ ServoAngleTest_90.ino
@@ -0,0 +1,42 @@
+#include <Wire.h>
+#include <Adafruit_PWMServoDriver.h>
+Adafruit_PWMServoDriver pwm =Adafruit_PWMServoDriver();
+#define SERVOMIN 150
+#define SERVOMAX 600
+
+int angleToPulse(int angle){
+  return map(angle, 0, 180,SERVOMIN, SERVOMAX);
+  }
+
+
+void setup() {
+  Serial.begin(9600);
+  Serial.println("servo test");
+  pwm.begin();
+  pwm.setPWMFreq(50);
+  delay(10);
+  pwm.setPWM(0, 0, angleToPulse(90));
+  delay(1000);
+
+}
+
+void loop() {
+  
+  for(int i=90;i<=150;i++){
+    pwm.setPWM(0, 0, angleToPulse(i));
+    delay(10);
+  }
+  delay(1000);
+
+  for(int i=150;i>=50;i--){
+    pwm.setPWM(0, 0, angleToPulse(i));
+    delay(10);
+  }
+  delay(1000);
+  for(int i=50;i<=90;i++){
+    pwm.setPWM(0, 0, angleToPulse(i));
+    delay(10);
+  }
+  
+
+}
